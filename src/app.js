365649ac72b8ae4b15e2d6eb0ac6814459ab5b2f
Nathan Lazo <84787060+NathanLazo@users.noreply.github.com>
2025-06-12 11:16:53 -0600

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const compression = require('compression');
-const all_routes = require('express-list-endpoints');
+const compression = require("compression");
+const all_routes = require("express-list-endpoints");
+const cors = require("cors");
 
-const logger = require('./utils/logger.js');
-const constants = require('./constants.js');
+const logger = require("./utils/logger.js");
+const constants = require("./constants.js");
 
 fileSizeLimit = constants.fileSizeLimit;
 timeout = 3600000;
@@ -12,66 +13,73 @@ timeout = 3600000;
 // catch SIGINT and SIGTERM and exit
 // Using a single function to handle multiple signals
 function handle(signal) {
-    logger.info(`Received ${signal}. Exiting...`);
-    process.exit(1)
-  }  
+  logger.info(`Received ${signal}. Exiting...`);
+  process.exit(1);
+}
 //SIGINT is typically CTRL-C
-process.on('SIGINT', handle);
+process.on("SIGINT", handle);
 //SIGTERM is sent to terminate process, for example docker stop sends SIGTERM
-process.on('SIGTERM', handle);
+process.on("SIGTERM", handle);
 
 app.use(compression());
 
+// Configuración de CORS
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://debor.ai"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
 //routes to handle file upload for all POST methods
-var upload = require('./routes/uploadfile.js');
+var upload = require("./routes/uploadfile.js");
 app.use(upload);
 
 //routes to convert audio/video/image files to mp3/mp4/jpg
-var convert = require('./routes/convert.js');
-app.use('/convert', convert);
+var convert = require("./routes/convert.js");
+app.use("/convert", convert);
 
 //routes to extract images or audio from video
-var extract = require('./routes/extract.js');
-app.use('/video/extract', extract);
+var extract = require("./routes/extract.js");
+app.use("/video/extract", extract);
 
 //routes to probe file info
-var probe = require('./routes/probe.js');
-app.use('/probe', probe);
+var probe = require("./routes/probe.js");
+app.use("/probe", probe);
 
-require('express-readme')(app, {
-    filename: 'index.md',
-    routes: ['/'],
+require("express-readme")(app, {
+  filename: "index.md",
+  routes: ["/"],
 });
 
-const server = app.listen(constants.serverPort, function() {
-    let host = server.address().address;
-    let port = server.address().port;
-    logger.info('Server started and listening http://'+host+':'+port)
+const server = app.listen(constants.serverPort, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+  logger.info("Server started and listening http://" + host + ":" + port);
 });
 
-server.on('connection', function(socket) {
-    logger.debug(`new connection, timeout: ${timeout}`);
-    socket.setTimeout(timeout);
-    socket.server.timeout = timeout;
-    server.keepAliveTimeout = timeout;
+server.on("connection", function (socket) {
+  logger.debug(`new connection, timeout: ${timeout}`);
+  socket.setTimeout(timeout);
+  socket.server.timeout = timeout;
+  server.keepAliveTimeout = timeout;
 });
 
-app.get('/endpoints', function(req, res) {
-    res.status(200).send(all_routes(app));
-    //res.writeHead(200, {'content-type' : 'text/plain'});
-    //res.end("Endpoints:\n\n"+JSON.stringify(all_routes(app),null,2)+'\n');
+app.get("/endpoints", function (req, res) {
+  res.status(200).send(all_routes(app));
+  //res.writeHead(200, {'content-type' : 'text/plain'});
+  //res.end("Endpoints:\n\n"+JSON.stringify(all_routes(app),null,2)+'\n');
 });
 
-app.use(function(req, res, next) {
-  res.status(404).send({error: 'route not found'});
+app.use(function (req, res, next) {
+  res.status(404).send({ error: "route not found" });
 });
-
 
 //custom error handler to return text/plain and message only
-app.use(function(err, req, res, next){
-    let code = err.statusCode || 500;
-    let message = err.message;
-    res.writeHead(code, {'content-type' : 'text/plain'});
-    res.end(`${err.message}\n`);
-    
+app.use(function (err, req, res, next) {
+  let code = err.statusCode || 500;
+  let message = err.message;
+  res.writeHead(code, { "content-type": "text/plain" });
+  res.end(`${err.message}\n`);
 });
